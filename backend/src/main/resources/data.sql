010696ee3a1667cd1735961ed575a01e55804264
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-05-28 18:36:29 -0300

    feat: add entity Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,3 +13,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course ( name,img_Uri,img_Gray_Uri) VALUES ('Bootcamp HTML','https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!','https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
